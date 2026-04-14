3c5016c59998c250c873137a7ab98a1fb3e04a39
GibusGaming <167337593+GibusGaming@users.noreply.github.com>
2026-04-14 18:05:36 +0300

    fixed critical issues
    Эта хуйня теперь ддолжна работать

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from tkinter import *
 from tkinter.ttk import *
 initiv_list = {}
 def initiaative_or_monster():
-    def __init__(self, initiative):
-        super().__init__(initiative)
-    @staticmethod
     def throw():
         global initiv_list
         beast = beast.get()
@@ -23,8 +20,8 @@ def initiaative_or_monster():
         cur.execute("SELECT initiva FROM monsters WHERE name = {beast}")
         initiative = cur.fetchone()
         advantage = advantage.get()
-        enemy.throw(initiative,advantage,beast,sign)
-        lbl.configure(text = dict(sorted(initiv_list.items(), key=lambda item: item[1])))
+        initiv_list = enemy.throw(initiative,advantage,beast,sign)
+        lbl.configure(root,text = dict(sorted(initiv_list.items(), key=lambda item: item[1])))
     tk = Tk()
     tk.title('Колесо инициативы для NPC')
     tk.geometry('400x100')
@@ -44,6 +41,11 @@ def initiaative_or_monster():
     btn.grid(column=1,row = 2)
     tk.mainloop()
 def initiative_for_player():
+    def dice_throw():
+        global initiv_list
+        name = name.get()
+        initiative = int(initiative.get()
+        initiv_list = player.throw(initiative,name)
     pl = Tk()
     pl.title('Колесо инициативы для игроков')
     pl.geometry('400x100')
@@ -55,9 +57,9 @@ def initiative_for_player():
     name.grid(column=0, row=1)
     lbl_name = Label(pl,text = 'Имя')
     lbl_name.grid(column=0,row=0)
-    btn = Button(pl,text='Ввести',command=player.throw)
+    btn = Button(pl,text='Ввести',command=dice_throw)
     btn.grid(column=0,row=2)
-    lbl.configure(text = dict(sorted(initiv_list.items(), key=lambda item: item[1])))
+    lbl.configure(root,text = dict(sorted(initiv_list.items(), key=lambda item: item[1])))
     pl.mainloop()
 root = Tk()
 root.geometry('200x100')
